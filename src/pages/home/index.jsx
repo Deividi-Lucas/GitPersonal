8e1c6bf70460b3d4fabfcdd8cdbc3285c0957f3c
Deividi-Lucas <deividibaby6@gmail.com>
2023-04-23 18:20:54 -0400

    Build: Config init, ready for coding

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,10 @@
+import { Main } from './style'
+
+export default function Home() {
+  return (
+    <Main>
+      <h1>Funcionando</h1>
+      <h2>Em desenvolvimento a página</h2>
+    </Main>
+  )
+}
