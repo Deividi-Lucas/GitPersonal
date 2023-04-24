a90ba92b7bdc0acf4a319af6f39024f98fe770fc
Deividi-Lucas <deividibaby6@gmail.com>
2023-04-24 16:30:08 -0400

    Feat: Home page ready <project finished>

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,59 @@
+import { useEffect, useState } from 'react'
 import { Main } from './style'
+import axios from 'axios'
+import { Divider, Input } from '@mui/material'
+import Header from '../../components/header'
+import gitphoto from '../../assets/git.png'
+import Footer from '../../components/footer'
 
 export default function Home() {
+  const [name, setName] = useState('Deividi-Lucas')
+  const [url, setUrl] = useState('')
+
+  useEffect(() => {
+    axios
+      .get(`https://api.github.com/users/${name}`)
+      .then(e => setUrl(e.data))
+      .catch(function (error) {
+        alert('Nome inválido')
+      })
+  }, [name])
+
   return (
-    <Main>
-      <h1>Funcionando</h1>
-      <h2>Em desenvolvimento a página</h2>
-    </Main>
+    <>
+      <Header title="GitPersonal" img={gitphoto} />
+      <Main>
+        <Input
+          placeholder="Pesquise aqui:"
+          onKeyUp={e => {
+            if (e.key == 'Enter') {
+              setName(e.target.value)
+            }
+          }}
+        />
+
+        <Divider orientation="horizontal" />
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column'
+          }}
+        >
+          <img
+            src={url?.avatar_url}
+            alt="Github_photo"
+            style={{ height: '3rem', width: '3rem', borderRadius: '1rem' }}
+          />
+          <span>Nome: {url.name}</span>
+          <span>Localidade: {url?.location}</span>
+
+          <span>Biografia: {url?.bio}</span>
+          <span>Blog: {url?.blog}</span>
+          <span>Seguidores: {url?.followers}</span>
+          <span>Repositórios público: {url?.public_repos}</span>
+        </div>
+      </Main>
+      <Footer />
+    </>
   )
 }
--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -1,3 +1,11 @@
 import styled from 'styled-components'
+import { color } from '../../utils/color'
 
-export const Main = styled.div``
+export const Main = styled.div`
+  background: ${color.roxoDark};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 80vh;
+  gap: 0.5rem;
+`
